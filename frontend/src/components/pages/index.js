ea4f1e43c8a33ef6dc284ec90ec1b4f87499a35d
L0GIC <cdhnet98@gmail.com>
2018-08-20 03:25:19 +0900

    Add index.js for pages

---
+++ frontend/src/components/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as Restaurant } from "./Restaurant";
+export { default as Login } from "./Login";
+export { default as Register } from "./Register";
+export { default as Like } from "./Like";
+export { default as Menu } from "./Menu";
