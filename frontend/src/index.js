d5cd0fbcaa8895f9d549aa3a53bbe5278b29d24a
L0GIC <cdhnet98@gmail.com>
2018-08-27 03:06:22 +0900

    Remove useless comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,4 @@ import App from "./App";
 
 /* 프로젝트 시작 7월 24일 ~ */
 
-/* 
-  배운점
-  아토믹 디자인
-  GraphQL
-  StyledComponent
-  Presentaitional / Container
-  구글 이미지 API - https://byeonghun-lee.github.io/2017/06/29/googleCustomSearch/
-*/
-
 ReactDOM.render(<App />, document.getElementById("root"));
