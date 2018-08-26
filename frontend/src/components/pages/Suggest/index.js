db46b302e2f26755728cd0b28f4dd9ba209f04d4
L0GIC <cdhnet98@gmail.com>
2018-08-27 08:01:30 +0900

    Add open chatting url

--- frontend/src/components/pages/Suggest/index.js
+++ frontend/src/components/pages/Suggest/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import styled from "styled-components";
 import { WidthLimiter, Navigation } from "../../commons";
 import { FormContainer } from "./containers";
 
@@ -7,10 +8,23 @@ const Suggest = () => {
     <Fragment>
       <WidthLimiter>
         <FormContainer />
+        <OpenChat>
+          또는 <a href="https://open.kakao.com/o/sP8bpJW">카카오톡 오픈채팅</a>
+          을 통해 채팅 문의가 가능합니다.
+        </OpenChat>
       </WidthLimiter>
       <Navigation active="menu" />
     </Fragment>
   );
 };
+
+const OpenChat = styled.div`
+  margin-top: 10px;
+  a {
+    color: #228be6;
+    text-decoration: underline;
+    font-weight: bold;
+  }
+`;
 
 export default Suggest;
